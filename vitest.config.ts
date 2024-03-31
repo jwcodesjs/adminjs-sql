b9068f778b14333d76abe110dfd189452b3c52d6
Jonathan Witchard <jwitchard@live.com>
2024-03-31 18:18:19 -0400

    chore: configure test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vitest/config';
 
+const isPostgres = process.env.DIALECT === 'postgresql'
+
 export default defineConfig({
   test: {
     globalSetup: ['src/test/setup.ts'],
     pool: 'forks',
     coverage: {
-      exclude: [],
+      exclude: [
+          '.eslintrc.cjs',
+          'src/test',
+          'commitlint.config.cjs',
+          `${isPostgres ? 'src/dialects/mysql.parser.ts' : 'src/dialects/postgres.parser.ts'}`,
+      ],
       clean: true,
       cleanOnRerun: true,
       provider: 'istanbul',
